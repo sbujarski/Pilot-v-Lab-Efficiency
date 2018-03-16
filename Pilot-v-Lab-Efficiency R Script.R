1799825b1e4b7f85636683203c5a3b7abba719b6
sbujarski <s.bujarski@gmail.com>
2018-03-15 22:50:11 -0700

    looping simulation and planning plot

--- Pilot-v-Lab-Efficiency R Script.R
+++ Pilot-v-Lab-Efficiency R Script.R
@@ -25,33 +25,46 @@ parameters$nlab <- parameters$npilot * parameters$LabMul
 
 #Want to run a simulation of 1000 meds for each parameter combination and compute the efficiency of pilot and lab based studies 
 #efficiency defined in terms of the number of medications that would receive a positive signal justifying a large Pilot
+nSims <- 100
+for (i in 1:dim(parameters)[1]){
+  #tracking simulations
+  print(noquote(paste("parameter combination ", i, "out of ", dim(parameters)[1])))
+  
+  SimData <- SimCor(n = nSims, 
+                    xmean = parameters$d[i], xsd = sqrt(des(d=parameters$d[i], n.1=parameters$npilot[i]/2, n.2=parameters$npilot[i]/2, verbose=F, dig=8)$var.d), 
+                    ymean = parameters$d[i], ysd = sqrt(des(d=parameters$d[i], n.1=parameters$nlab[i]/2, n.2=parameters$nlab[i]/2, verbose=F, dig=8)$var.d),
+                    rho = parameters$r.LabPilot[i])
+  SimData <- rename(SimData, dPilot = x, dLab = y)
 
-#testing with i = 1
-nSims <- 1000
-SimData <- SimCor(n = nSims, 
-                  xmean = parameters$d[i], xsd = sqrt(des(d=parameters$d[i], n.1=parameters$npilot[i]/2, n.2=parameters$npilot[i]/2, verbose=F, dig=8)$var.d), 
-                  ymean = parameters$d[i], ysd = sqrt(des(d=parameters$d[i], n.1=parameters$nlab[i]/2, n.2=parameters$nlab[i]/2, verbose=F, dig=8)$var.d),
-                  rho = parameters$r.LabPilot[i])
-SimData <- rename(SimData, dPilot = x, dLab = y)
-SpDesc(SimData)
-# SpDesc(SimData)
-# cor.test(~ dPilot + dLab, data=SimData)
-#use lapply to get the sqrt of each dPilot and dLab
-SimData$dPilot.SD <- as.double(lapply(SimData$dPilot, function(x) sqrt(des(d=x, n.1=parameters$npilot[i]/2, n.2=parameters$npilot[i]/2, verbose=F, dig=8)$var.d)))
-SimData$dLab.SD <- as.double(lapply(SimData$dLab, function(x) sqrt(des(d=x, n.1=parameters$nlab[i]/2, n.2=parameters$nlab[i]/2, verbose=F, dig=8)$var.d)))
+  #use lapply to get the sqrt of each dPilot and dLab
+  SimData$dPilot.SD <- as.double(lapply(SimData$dPilot, function(x) sqrt(des(d=x, n.1=parameters$npilot[i]/2, n.2=parameters$npilot[i]/2, verbose=F, dig=8)$var.d)))
+  SimData$dLab.SD <- as.double(lapply(SimData$dLab, function(x) sqrt(des(d=x, n.1=parameters$nlab[i]/2, n.2=parameters$nlab[i]/2, verbose=F, dig=8)$var.d)))
+  
+  #get p-values from d's
+  SimData$dPilot.p <- as.double(lapply(SimData$dPilot, function(x) des(d=x, n.1=parameters$npilot[i]/2, n.2=parameters$npilot[i]/2, verbose=F, dig=8)$pval.d))
+  SimData$dLab.p <- as.double(lapply(SimData$dLab, function(x) des(d=x, n.1=parameters$nlab[i]/2, n.2=parameters$nlab[i]/2, verbose=F, dig=8)$pval.d))
 
-#get p-values from d's
-SimData$dPilot.p <- as.double(lapply(SimData$dPilot, function(x) des(d=x, n.1=parameters$npilot[i]/2, n.2=parameters$npilot[i]/2, verbose=F, dig=8)$pval.d))
-SimData$dLab.p <- as.double(lapply(SimData$dLab, function(x) des(d=x, n.1=parameters$nlab[i]/2, n.2=parameters$nlab[i]/2, verbose=F, dig=8)$pval.d))
+  #save the power of Pilot and associated lab studies
+  parameters$EffPilot[i] <- mean(SimData$dPilot.p<0.05)
+  parameters$EffLab[i] <- mean(SimData$dLab.p<0.05)
+}
+
+View(parameters)
+
+#plotting----
+#Dont need to plot the pilot repeated for each LabMul
+parameters.pilot <- subset(parameters, LabMul == 2)
+Eff.plot <- ggplot(data=parameters, aes(x=npilot, y=EffLab, colour=as.factor(LabMul))) +
+  facet_grid(as.factor(r.LabPilot) ~ as.factor(d)) +
+  geom_line() + 
+  geom_line(data=parameters.pilot, aes(x=npilot, y=EffPilot), colour = "black", size=2) +
+  DotRTheme()
+Eff.plot
 
 
 # SpHist(data=SimData, variable = "dPilot")
 # SpHist(data=SimData, variable = "dLab")
 # SpHist(data=SimData, variable = "dPilot.p")
 # SpHist(data=SimData, variable = "dLab.p")
-
-parameters$EffPilot <- mean(SimData$dPilot.p<0.05)
-parameters$EffLab <- mean(SimData$dLab.p<0.05)
-
 
 
