aec90d06900dbb9fa36cc7988b7f97eaea215d85
sbujarski <s.bujarski@gmail.com>
2018-03-15 21:54:09 -0700

    renaming RCT to pilot for consistency

--- Pilot-v-Lab-Efficiency R Script.R
+++ Pilot-v-Lab-Efficiency R Script.R
@@ -14,42 +14,47 @@ library(MBESS)
 #d = c(0.2, 0.5, 0.8) -- small, medium and large canonical Cohen's d
 #npilot = seq(6,36,6) -- sample size of pilot test from 6 to 36
 #LabMul = seq(2,10,2) -- Lab multiple for sample size associated with laboratory studies being considerably cheaper to run
-#r.LabRCT = seq(0.3,0.9,0.2) -- correlation between laboratory-based effect size and the RCT effect size
+#r.LabPilot = seq(0.3,0.9,0.2) -- correlation between laboratory-based effect size and the Pilot effect size
 
 parameters <- expand.grid(d = c(0.2, 0.5, 0.8), 
                          npilot = seq(6,36,6),
                          LabMul = seq(2,10,2),
-                         r.LabRCT = seq(0.3,0.9,0.2))
-parameters <- arrange(parameters, d, npilot, LabMul, r.LabRCT)
+                         r.LabPilot = seq(0.3,0.9,0.2))
+parameters <- arrange(parameters, d, npilot, LabMul, r.LabPilot)
+parameters$nlab <- parameters$npilot * parameters$LabMul
 
 #compute SD of d
 for (i in 1:dim(parameters)[1]){
   parameters$d.SD <- sqrt(des(d=parameters$d[i], n.1=parameters$npilot/2, n.2=parameters$npilot/2, verbose=F, dig=8)$var.d)
 }
 
+
 #Want to run a simulation of 1000 meds for each parameter combination and compute the efficiency of pilot and lab based studies 
-#efficiency defined in terms of the number of medications that would receive a positive signal justifying a large RCT
+#efficiency defined in terms of the number of medications that would receive a positive signal justifying a large Pilot
 
 #testing with i = 1
 nSims <- 1000
 SimData <- SimCor(n = nSims, 
                   xmean = parameters$d[i], xsd = parameters$d.SD[i], 
                   ymean = parameters$d[i], ysd = parameters$d.SD[i],
-                  rho = parameters$r.LabRCT[i])
-SimData <- rename(SimData, dRCT = x, dLab = y)
+                  rho = parameters$r.LabPilot[i])
+SimData <- rename(SimData, dPilot = x, dLab = y)
 # SpDesc(SimData)
-# cor.test(~ dRCT + dLab, data=SimData)
-#use lapply to get the sqrt of each dRCT and dLab
-SimData$dRCT.SD <- as.double(lapply(SimData$dRCT, function(x) sqrt(des(d=x, n.1=parameters$npilot[i]/2, n.2=parameters$npilot[i]/2, verbose=F, dig=8)$var.d)))
-SimData$dLab.SD <- as.double(lapply(SimData$dLab, function(x) sqrt(des(d=x, n.1=parameters$npilot[i]/2, n.2=parameters$npilot[i]/2, verbose=F, dig=8)$var.d)))
+# cor.test(~ dPilot + dLab, data=SimData)
+#use lapply to get the sqrt of each dPilot and dLab
+SimData$dPilot.SD <- as.double(lapply(SimData$dPilot, function(x) sqrt(des(d=x, n.1=parameters$npilot[i]/2, n.2=parameters$npilot[i]/2, verbose=F, dig=8)$var.d)))
+SimData$dLab.SD <- as.double(lapply(SimData$dLab, function(x) sqrt(des(d=x, n.1=parameters$nlab[i]/2, n.2=parameters$nlab[i]/2, verbose=F, dig=8)$var.d)))
 
 #get p-values from d's
-SimData$dRCT.p <- as.double(lapply(SimData$dRCT, function(x) des(d=x, n.1=parameters$npilot[i]/2, n.2=parameters$npilot[i]/2, verbose=F, dig=8)$pval.d))
+SimData$dPilot.p <- as.double(lapply(SimData$dPilot, function(x) des(d=x, n.1=parameters$npilot[i]/2, n.2=parameters$npilot[i]/2, verbose=F, dig=8)$pval.d))
 SimData$dLab.p <- as.double(lapply(SimData$dLab, function(x) des(d=x, n.1=parameters$npilot[i]/2, n.2=parameters$npilot[i]/2, verbose=F, dig=8)$pval.d))
 
 
-SpHist(data=SimData, variable = "dRCT")
+SpHist(data=SimData, variable = "dPilot")
 SpHist(data=SimData, variable = "dLab")
-SpHist(data=SimData, variable = "dRCT.p")
+SpHist(data=SimData, variable = "dPilot.p")
 SpHist(data=SimData, variable = "dLab.p")
+
+mean(SimData$dPilot.p<0.05)
+mean(SimData$dLab.p<0.05)
 
