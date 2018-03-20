77f8aae8d9f1577e8be2a7cb2a5fb1f78d844640
sbujarski <s.bujarski@gmail.com>
2018-03-20 13:18:09 -0700

    simulating medications + power

--- Pilot-v-Lab-Efficiency R Script.R
+++ Pilot-v-Lab-Efficiency R Script.R
@@ -9,6 +9,7 @@ library(gmodels) #CrossTable Function
 library(Hmisc) #rcorr
 library(compute.es)
 library(MBESS)
+library(pwr)
 
 #parameters to test
 #d = c(0.2, 0.5, 0.8) -- small, medium and large canonical Cohen's d
@@ -16,40 +17,61 @@ library(MBESS)
 #LabMul = seq(2,10,2) -- Lab multiple for sample size associated with laboratory studies being considerably cheaper to run
 #r.LabPilot = seq(0.3,0.9,0.2) -- correlation between laboratory-based effect size and the Pilot effect size
 
-parameters <- expand.grid(d = c(0.2, 0.5, 0.8), 
-                         npilot = seq(6,36,6),
-                         LabMul = seq(1,4,1),
-                         r.LabPilot = seq(0.3,0.9,0.2))
-parameters <- arrange(parameters, d, npilot, LabMul, r.LabPilot)
+parameters <- expand.grid(npilot = seq(6,36,6),
+                         LabMul = c(1,2,4),
+                         r.LabPilot = seq(0.3,0.9,0.3))
+parameters <- arrange(parameters, npilot, LabMul, r.LabPilot)
 parameters$nlab <- parameters$npilot * parameters$LabMul
 
 #Want to run a simulation of 1000 meds for each parameter combination and compute the efficiency of pilot and lab based studies 
 #efficiency defined in terms of the number of medications that would receive a positive signal justifying a large Pilot
-nSims <- 1000
+nMeds <- 10
 for (i in 1:dim(parameters)[1]){
   #tracking simulations
   print(noquote(paste("parameter combination ", i, "out of ", dim(parameters)[1])))
   
-  SimData <- SimCor(n = nSims, 
-                    xmean = parameters$d[i], xsd = sqrt(des(d=parameters$d[i], n.1=parameters$npilot[i]/2, n.2=parameters$npilot[i]/2, verbose=F, dig=8)$var.d), 
-                    ymean = parameters$d[i], ysd = sqrt(des(d=parameters$d[i], n.1=parameters$nlab[i]/2, n.2=parameters$nlab[i]/2, verbose=F, dig=8)$var.d),
-                    rho = parameters$r.LabPilot[i])
-  SimData <- rename(SimData, dPilot = x, dLab = y)
-
-  #use lapply to get the sqrt of each dPilot and dLab
-  SimData$dPilot.SD <- as.double(lapply(SimData$dPilot, function(x) sqrt(des(d=x, n.1=parameters$npilot[i]/2, n.2=parameters$npilot[i]/2, verbose=F, dig=8)$var.d)))
-  SimData$dLab.SD <- as.double(lapply(SimData$dLab, function(x) sqrt(des(d=x, n.1=parameters$nlab[i]/2, n.2=parameters$nlab[i]/2, verbose=F, dig=8)$var.d)))
+  #Simulate nMeds of each d (0, 0.2, 0.5, 0.8) with SD = 0.2
+  #Simulate simultaneously the medication lab efficacy based on parameters$r.LabPilot[i]
+  SimMeds.d0 <- data.frame(dgroup=0, SimCor(n = nMeds, xmean=0, xsd=0.2, ymean=0, ysd=0.2, rho=parameters$r.LabPilot[i]))
+  SimMeds.d0 <- rename(SimMeds.d0, dPilot = x, dLab = y)
+  SimMeds.d2 <- data.frame(dgroup=0.2, SimCor(n = nMeds, xmean=0.2, xsd=0.2, ymean=0.2, ysd=0.2, rho=parameters$r.LabPilot[i]))
+  SimMeds.d2 <- rename(SimMeds.d2, dPilot = x, dLab = y)
+  SimMeds.d5 <- data.frame(dgroup=0.5, SimCor(n = nMeds, xmean=0.5, xsd=0.2, ymean=0.5, ysd=0.2, rho=parameters$r.LabPilot[i]))
+  SimMeds.d5 <- rename(SimMeds.d5, dPilot = x, dLab = y)
+  SimMeds.d8 <- data.frame(dgroup=0.8, SimCor(n = nMeds, xmean=0.8, xsd=0.2, ymean=0.8, ysd=0.2, rho=parameters$r.LabPilot[i]))
+  SimMeds.d8 <- rename(SimMeds.d8, dPilot = x, dLab = y)
   
-  #get p-values from d's
-  SimData$dPilot.p <- as.double(lapply(SimData$dPilot, function(x) des(d=x, n.1=parameters$npilot[i]/2, n.2=parameters$npilot[i]/2, verbose=F, dig=8)$pval.d))
-  SimData$dLab.p <- as.double(lapply(SimData$dLab, function(x) des(d=x, n.1=parameters$nlab[i]/2, n.2=parameters$nlab[i]/2, verbose=F, dig=8)$pval.d))
-
-  #save the power of Pilot and associated lab studies
-  parameters$EffPilot[i] <- mean(SimData$dPilot.p<0.05)
+  #merge Simulated meds
+  SimMeds <- rbind(SimMeds.d0, SimMeds.d2, SimMeds.d5, SimMeds.d8)
   
-  #How to account for the fact that a significant lab result might be a false positive
-  #Only count a significant Lab test as "efficient positive screen" if the SimData$dPilot which is the medication RCT effect size is >= 0.2
-  parameters$EffLab[i] <- sum(subset(SimData, dPilot >= 0.2)$dLab.p<0.05)/nSims
+  #SimMeds %>% group_by(as.factor(dgroup)) %>% summarise(meandPilot = mean(dPilot), sddPilot = sd(dPilot), meandLab = mean(dLab), sddLab = sd(dLab))  
+  
+  #compute power for each medication in pilot study and in lab
+  
+  
+  
+  
+  
+  # SimData <- SimCor(n = nSims, 
+  #                   xmean = parameters$d[i], xsd = sqrt(des(d=parameters$d[i], n.1=parameters$npilot[i]/2, n.2=parameters$npilot[i]/2, verbose=F, dig=8)$var.d), 
+  #                   ymean = parameters$d[i], ysd = sqrt(des(d=parameters$d[i], n.1=parameters$nlab[i]/2, n.2=parameters$nlab[i]/2, verbose=F, dig=8)$var.d),
+  #                   rho = parameters$r.LabPilot[i])
+  # SimData <- rename(SimData, dPilot = x, dLab = y)
+  # 
+  # #use lapply to get the sqrt of each dPilot and dLab
+  # SimData$dPilot.SD <- as.double(lapply(SimData$dPilot, function(x) sqrt(des(d=x, n.1=parameters$npilot[i]/2, n.2=parameters$npilot[i]/2, verbose=F, dig=8)$var.d)))
+  # SimData$dLab.SD <- as.double(lapply(SimData$dLab, function(x) sqrt(des(d=x, n.1=parameters$nlab[i]/2, n.2=parameters$nlab[i]/2, verbose=F, dig=8)$var.d)))
+  # 
+  # #get p-values from d's
+  # SimData$dPilot.p <- as.double(lapply(SimData$dPilot, function(x) des(d=x, n.1=parameters$npilot[i]/2, n.2=parameters$npilot[i]/2, verbose=F, dig=8)$pval.d))
+  # SimData$dLab.p <- as.double(lapply(SimData$dLab, function(x) des(d=x, n.1=parameters$nlab[i]/2, n.2=parameters$nlab[i]/2, verbose=F, dig=8)$pval.d))
+  # 
+  # #save the power of Pilot and associated lab studies
+  # parameters$EffPilot[i] <- mean(SimData$dPilot.p<0.05)
+  # 
+  # #How to account for the fact that a significant lab result might be a false positive
+  # #Only count a significant Lab test as "efficient positive screen" if the SimData$dPilot which is the medication RCT effect size is >= 0.2
+  # parameters$EffLab[i] <- sum(subset(SimData, dPilot >= 0.2)$dLab.p<0.05)/nSims
 }
 
 View(parameters)
