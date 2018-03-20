17cf2b72e2caba3d283198a0ec6a07c12845afe3
sbujarski <s.bujarski@gmail.com>
2018-03-20 14:46:26 -0700

    switching to mvrNorm from MASS for simulating correlated data

--- Pilot-v-Lab-Efficiency R Script.R
+++ Pilot-v-Lab-Efficiency R Script.R
@@ -10,22 +10,23 @@ library(Hmisc) #rcorr
 library(compute.es)
 library(MBESS)
 library(pwr)
+library(Mass)
 
 #parameters to test
 #d = c(0.2, 0.5, 0.8) -- small, medium and large canonical Cohen's d
-#npilot = seq(6,36,6) -- sample size of pilot test from 6 to 36
+#nPilot = seq(6,36,6) -- sample size of pilot test from 6 to 36
 #LabMul = seq(2,10,2) -- Lab multiple for sample size associated with laboratory studies being considerably cheaper to run
 #r.LabPilot = seq(0.3,0.9,0.2) -- correlation between laboratory-based effect size and the Pilot effect size
 
-parameters <- expand.grid(npilot = seq(6,36,6),
+parameters <- expand.grid(nPilot = seq(6,36,6),
                          LabMul = c(1,2,4),
                          r.LabPilot = seq(0.3,0.9,0.3))
-parameters <- arrange(parameters, npilot, LabMul, r.LabPilot)
-parameters$nlab <- parameters$npilot * parameters$LabMul
+parameters <- arrange(parameters, nPilot, LabMul, r.LabPilot)
+parameters$nLab <- parameters$nPilot * parameters$LabMul
 
 #Want to run a simulation of 1000 meds for each parameter combination and compute the efficiency of pilot and lab based studies 
 #efficiency defined in terms of the number of medications that would receive a positive signal justifying a large Pilot
-nMeds <- 10
+nMeds <- 1000
 for (i in 1:dim(parameters)[1]){
   #tracking simulations
   print(noquote(paste("parameter combination ", i, "out of ", dim(parameters)[1])))
@@ -43,53 +44,80 @@ for (i in 1:dim(parameters)[1]){
   
   #merge Simulated meds
   SimMeds <- rbind(SimMeds.d0, SimMeds.d2, SimMeds.d5, SimMeds.d8)
+  #testing
+  SimMeds %>% group_by(as.factor(dgroup)) %>% summarise(meandPilot = mean(dPilot), sddPilot = sd(dPilot), 
+                                                        meandLab = mean(dLab), sddLab = sd(dLab))
   
   #SimMeds %>% group_by(as.factor(dgroup)) %>% summarise(meandPilot = mean(dPilot), sddPilot = sd(dPilot), meandLab = mean(dLab), sddLab = sd(dLab))  
   
-  #compute power for each medication in pilot study and in lab
+  # #use lapply to compute power for each medication in pilot study and in lab
+  #need to use one sided because a negative effect size being detected isn't a positive screen
+  SimMeds$power.Pilot <- as.double(lapply(SimMeds$dPilot, function(x) pwr.t.test(n=(parameters$nPilot[i]/2), d=x, sig.level=0.025, 
+                                                                                 type="two.sample", alternative="greater")$power))
+  SimMeds$power.Lab <- as.double(lapply(SimMeds$dLab, function(x) pwr.t.test(n=(parameters$nLab[i]/2), d=x, sig.level=0.025, 
+                                                                                 type="two.sample", alternative="greater")$power))
   
+  SimMeds.d0$power.Pilot <- as.double(lapply(SimMeds.d0$dPilot, function(x) pwr.t.test(n=(parameters$nPilot[i]/2), d=x, sig.level=0.025,
+                                                                                   type="two.sample", alternative="greater")$power))
+  SimMeds.d0$power.Lab <- as.double(lapply(SimMeds.d0$dLab, function(x) pwr.t.test(n=(parameters$nLab[i]/2), d=x, sig.level=0.025,
+                                                                             type="two.sample", alternative="greater")$power))
+  SimMeds %>% group_by(as.factor(dgroup)) %>% summarise(meanpowerpilot = mean(power.Pilot), meanpowerlab = mean(power.Lab))
+  ggplot(data=SimMeds.d0, aes(x=dPilot, y=power.Pilot)) + geom_point() + scale_y_continuous(limits=c(0,1)) + scale_x_continuous(limits=c(-1,1))
+  ggplot(data=SimMeds.d0, aes(x=dLab, y=power.Lab)) + geom_point() + scale_y_continuous(limits=c(0,1)) + scale_x_continuous(limits=c(-1,1))
+
+  #calculate expected number of positive findings by effect size group
+  positives <- SimMeds %>% group_by(as.factor(dgroup)) %>% summarise(posPilot = sum(power.Pilot), posLab = sum(power.Lab))  
   
+  #calculate the efficiency
+  #defined as number of positive screens from pilot/lab studies as a multiple as those with 0 effect size
+  #Pilot Efficiencies
+  parameters$eff.Pilot.d2[i] <- positives$posPilot[2] / positives$posPilot[1]
+  parameters$eff.Pilot.d5[i] <- positives$posPilot[3] / positives$posPilot[1]
+  parameters$eff.Pilot.d8[i] <- positives$posPilot[4] / positives$posPilot[1]
   
-  
-  
-  # SimData <- SimCor(n = nSims, 
-  #                   xmean = parameters$d[i], xsd = sqrt(des(d=parameters$d[i], n.1=parameters$npilot[i]/2, n.2=parameters$npilot[i]/2, verbose=F, dig=8)$var.d), 
-  #                   ymean = parameters$d[i], ysd = sqrt(des(d=parameters$d[i], n.1=parameters$nlab[i]/2, n.2=parameters$nlab[i]/2, verbose=F, dig=8)$var.d),
-  #                   rho = parameters$r.LabPilot[i])
-  # SimData <- rename(SimData, dPilot = x, dLab = y)
-  # 
-  # #use lapply to get the sqrt of each dPilot and dLab
-  # SimData$dPilot.SD <- as.double(lapply(SimData$dPilot, function(x) sqrt(des(d=x, n.1=parameters$npilot[i]/2, n.2=parameters$npilot[i]/2, verbose=F, dig=8)$var.d)))
-  # SimData$dLab.SD <- as.double(lapply(SimData$dLab, function(x) sqrt(des(d=x, n.1=parameters$nlab[i]/2, n.2=parameters$nlab[i]/2, verbose=F, dig=8)$var.d)))
-  # 
-  # #get p-values from d's
-  # SimData$dPilot.p <- as.double(lapply(SimData$dPilot, function(x) des(d=x, n.1=parameters$npilot[i]/2, n.2=parameters$npilot[i]/2, verbose=F, dig=8)$pval.d))
-  # SimData$dLab.p <- as.double(lapply(SimData$dLab, function(x) des(d=x, n.1=parameters$nlab[i]/2, n.2=parameters$nlab[i]/2, verbose=F, dig=8)$pval.d))
-  # 
-  # #save the power of Pilot and associated lab studies
-  # parameters$EffPilot[i] <- mean(SimData$dPilot.p<0.05)
-  # 
-  # #How to account for the fact that a significant lab result might be a false positive
-  # #Only count a significant Lab test as "efficient positive screen" if the SimData$dPilot which is the medication RCT effect size is >= 0.2
-  # parameters$EffLab[i] <- sum(subset(SimData, dPilot >= 0.2)$dLab.p<0.05)/nSims
+  #Lab Efficiencies
+  parameters$eff.Lab.d2[i] <- positives$posLab[2] / positives$posLab[1]
+  parameters$eff.Lab.d5[i] <- positives$posLab[3] / positives$posLab[1]
+  parameters$eff.Lab.d8[i] <- positives$posLab[4] / positives$posLab[1]
 }
 
 View(parameters)
 
 #plotting----
-#Dont need to plot the pilot repeated for each LabMul
-parameters.pilot <- subset(parameters, LabMul == 2)
+#average pilot efficiency for each nPilot
+PilotEfficiency <- parameters %>% group_by(nPilot) %>% summarise(meaneff.Pilot.d2 = mean(eff.Pilot.d2),
+                                                                 meaneff.Pilot.d5 = mean(eff.Pilot.d5),
+                                                                 meaneff.Pilot.d8 = mean(eff.Pilot.d8))
 
-colorscale <- scales::seq_gradient_pal("lightblue", "navyblue", "Lab")(seq(0,1,length.out=4))
-Eff.plot <- ggplot(data=parameters, aes(x=npilot, y=EffLab, colour=as.factor(LabMul))) +
-  facet_grid(paste("Lab-RCT r = ", r.LabPilot) ~ paste("Cohen's d = ",d)) +
+colorscale <- scales::seq_gradient_pal("lightblue", "navyblue", "Lab")(seq(0,1,length.out=3))
+Eff.plot.d2 <- ggplot(data=parameters, aes(x=nPilot, y=eff.Lab.d2, colour=as.factor(r.LabPilot))) +
+  facet_grid(LabMul ~ .) +
   geom_line() + 
-  scale_colour_manual("Lab Sample\nMultiple", values=colorscale) +
-  geom_line(data=parameters.pilot, aes(x=npilot, y=EffPilot), colour = "black", size=2) +
+  geom_line(data=PilotEfficiency, aes(x=nPilot, y=meaneff.Pilot.d2), colour = "black", size=2) +
+  scale_colour_manual("Lab-Pilot\nCorrelation", values=colorscale) +
   theme_bw() + 
   theme(panel.border = element_rect(color = "black", fill=NA))
-Eff.plot
+Eff.plot.d2
 
+colorscale <- scales::seq_gradient_pal("lightblue", "navyblue", "Lab")(seq(0,1,length.out=3))
+Eff.plot.d5 <- ggplot(data=parameters, aes(x=nPilot, y=eff.Lab.d5, colour=as.factor(r.LabPilot))) +
+  facet_grid(LabMul ~ .) +
+  geom_line() + 
+  geom_line(data=PilotEfficiency, aes(x=nPilot, y=meaneff.Pilot.d5), colour = "black", size=2) +
+  scale_colour_manual("Lab-Pilot\nCorrelation", values=colorscale) +
+  theme_bw() + 
+  theme(panel.border = element_rect(color = "black", fill=NA))
+Eff.plot.d5
+
+colorscale <- scales::seq_gradient_pal("lightblue", "navyblue", "Lab")(seq(0,1,length.out=3))
+Eff.plot.d8 <- ggplot(data=parameters, aes(x=nPilot, y=eff.Lab.d8, colour=as.factor(r.LabPilot))) +
+  facet_grid(LabMul ~ .) +
+  geom_line() + 
+  geom_line(data=PilotEfficiency, aes(x=nPilot, y=meaneff.Pilot.d8), colour = "black", size=2) +
+  scale_colour_manual("Lab-Pilot\nCorrelation", values=colorscale) +
+  theme_bw() + 
+  theme(panel.border = element_rect(color = "black", fill=NA))
+Eff.plot.d8
 
 # SpHist(data=SimData, variable = "dPilot")
 # SpHist(data=SimData, variable = "dLab")
